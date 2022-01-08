bdf3d311fd30bacb8c5ce2c5178232ac11659f1d
mohammedumair123 <kasarumair786@gmail.com>
2022-01-08 20:59:23 +0530

    umair-update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Rizwan-Folder/Nav';
+import { Zaki } from './Components/Umair-Folder/Zaki';
 
 function App() {
   return (
     <div className="App">
      <Nav/>
+     <Zaki/>
     </div>
   );
 }
---
+++ src/Components/Umair-Folder/Zaki.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Zaki = () => {
+  return (
+    <div>
+      <h1>Good</h1>
+    </div>
+  );
+};
